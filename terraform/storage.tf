97499c2e68b6e8d06ce9c19d834540e0421ef8d1
Eric Crossman <eric_crossman@cox.net>
2018-11-16 13:01:45 -0500

    Adding storage account resource for storing diag logs.

---
+++ terraform/storage.tf
@@ -0,0 +1,21 @@
+resource "random_id" "randomId" {
+  keepers = {
+    # Generate a new ID only when a resource group is defined
+    resource_group = "${azurerm_resource_group.puppetdemo.name}"
+  }
+
+  byte_length = 8
+}
+
+resource "azurerm_storage_account" "diagstorageaccount" {
+  name = "diag${$random_id.randomId.hex}"
+  resource_group_name = "${azurerm_resource_group.puppetdemo.name}"
+  location = "eastus"
+  account_replication_type = "LRS"
+  account_tier = "Standard"
+
+  tags {
+    environment = "demo"
+    ProjectId = "PuppetEnterpriseDemo"
+  }
+}
