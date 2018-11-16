ff55df0559156af2b2f24c13835739a6f7826a4f
Eric Crossman <eric_crossman@cox.net>
2018-11-16 12:27:16 -0500

    Correction for missing PuppetMasterIP parameter.

--- scripts/deployEnvironment.bat
+++ scripts/deployEnvironment.bat
@@ -1,6 +1,6 @@
 aws cloudformation deploy --template packaged-template.json --stack-name PuppetEnterpriseDemo \
 --parameter-overrides HostedZoneName=puppet.ericcrossman.com \
 InstanceType=m4.large KeyPairName=UdemyCourseworkNorthVirginia_public \
-MasterHostName=puppet PuppetMasterDNSName=master RemoteAdminCIDR=128.227.238.96/32 \
+MasterHostName=puppet PuppetMasterDNSName=master PuppetMasterIP=192.168.1.10 RemoteAdminCIDR=128.227.189.47/32 \
 SubnetCIDR=192.168.1.0/24 VPCCIDR=192.168.0.0/22 \
 --tags ProjectId=PuppetEnterpriseEval PEVersion=2017.3.5 --capabilities CAPABILITY_IAM
