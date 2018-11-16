9309b05261c1100013f34120a436579fdf2ed235
Eric Crossman <eric_crossman@cox.net>
2018-11-16 12:35:16 -0500

    Adding enclosing resource group for all resources in this project.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,13 @@
 provider "azurerm" {
 
 }
+
+resource "azurerm_resource_group" "puppetdemo" {
+  name = "puppetdemo"
+  location = "eastus"
+
+  tags = {
+    environment = "demo"
+    ProjectId = "PuppetEnterpriseEval"
+  }
+}
