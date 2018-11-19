9fcbb1c287f3b147cb97e8aa46b9ef046a4f8906
Eric Crossman <eric_crossman@cox.net>
2018-11-19 15:19:26 -0500

    Adding a dns label for ease of console access.

--- terraform/network.tf
+++ terraform/network.tf
@@ -19,6 +19,7 @@ resource "azurerm_subnet" "puppetsubnet" {
 
 resource "azurerm_public_ip" "PuppetMasterPublicIP" {
   name = "PuppetMasterPublicIP"
+  domain_name_label = "ercpuppet"
   location = "eastus"
   resource_group_name = "${azurerm_resource_group.puppetdemo.name}"
   public_ip_address_allocation = "dynamic"
