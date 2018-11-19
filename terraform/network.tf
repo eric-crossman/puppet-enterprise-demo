f632a4b7dd2491102d764d87a58396a7f1aea857
Eric Crossman <eric_crossman@cox.net>
2018-11-19 15:24:45 -0500

    Adding dedicated public ip for MediaWiki instance.

--- terraform/network.tf
+++ terraform/network.tf
@@ -17,6 +17,19 @@ resource "azurerm_subnet" "puppetsubnet" {
   address_prefix = "192.168.72.0/24"
 }
 
+resource "azurerm_public_ip" "MediaWiki01PublicIP" {
+  name = "MediaWiki01PublicIP"
+  domain_name_label = "mediawiki01"
+  location = "eastus"
+  resource_group_name = "${azurerm_resource_group.puppetdemo.name}"
+  public_ip_address_allocation = "dynamic"
+
+  tags = {
+    environment = "demo"
+    ProjectId = "PuppetEnterpriseDemo"
+  }
+}
+
 resource "azurerm_public_ip" "PuppetMasterPublicIP" {
   name = "PuppetMasterPublicIP"
   domain_name_label = "ercpuppet"
